91269dbf57f6e61980ed4525f349541cb7e5ab72
Louis <louis-kaan2002@web.de>
2023-03-05 18:57:17 +0100

    added jest and supertest

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,8 @@
-import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
-import cors from 'cors';
-import helmet from 'helmet';
-import compression from 'compression';
+import app from './app';
 import logger from './utils/logger';
-import shouldCompress from './utils/shouldCompress';
 
 dotenv.config();
-
-const app: Express = express();
-
-app.use(
-  cors({
-    // origin is given a array if we want to have multiple origins later
-    origin: [process.env.CORS_ORIGIN],
-    credentials: true,
-  })
-);
-
-// Helmet is used to secure this app by configuring the http-header
-app.use(helmet());
-
-// Compression is used to reduce the size of the response body
-app.use(compression({ filter: shouldCompress }));
-
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!');
-});
 
 app.listen(parseInt(process.env.PORT), () => {
   logger.log('info', `Server is running on Port: ${process.env.PORT}`);
