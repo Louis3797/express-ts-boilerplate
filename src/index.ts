9517449af97f6b2bd71acf35de35de230f0742e1
Louis <louis-kaan2002@web.de>
2023-03-06 18:35:14 +0100

    removed dotenv config from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import * as dotenv from 'dotenv';
 import app from './app';
 import logger from './middleware/logger';
-
-dotenv.config();
 
 app.listen(parseInt(process.env.PORT), () => {
   logger.log('info', `Server is running on Port: ${process.env.PORT}`);
